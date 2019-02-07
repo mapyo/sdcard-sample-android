0ab6dd30896453831f04e09ef458efb7c91a5805
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2019-02-07 11:19:36 +0900

    use databinding

--- app/src/main/java/com/mapyo/sdcard_sample_android/MainActivity.kt
+++ app/src/main/java/com/mapyo/sdcard_sample_android/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.mapyo.sdcard_sample_android
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.mapyo.sdcard_sample_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.requestPermissionButton.setOnClickListener {
+            Toast.makeText(this , "hoge", Toast.LENGTH_SHORT).show();
+        }
     }
 }
