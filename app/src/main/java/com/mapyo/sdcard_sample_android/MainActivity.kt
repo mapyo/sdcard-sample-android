8f08bd42d0974ad95b4827ba9b2c9a1809a24d8f
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2019-02-12 11:38:37 +0900

    feat: open picker

--- app/src/main/java/com/mapyo/sdcard_sample_android/MainActivity.kt
+++ app/src/main/java/com/mapyo/sdcard_sample_android/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.mapyo.sdcard_sample_android
 
+import android.app.Activity
+import android.app.Instrumentation
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.mapyo.sdcard_sample_android.databinding.ActivityMainBinding
+import android.content.Intent
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +21,31 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.requestPermissionButton.setOnClickListener {
-            Toast.makeText(this , "hoge", Toast.LENGTH_SHORT).show();
+            requestSdcardAccessPermission()
         }
+    }
+
+    // 5系でのみの許可を得る方法
+    // TODO 事前にピッカーで指定された操作をやってもらうようにする必要がある。
+    private fun requestSdcardAccessPermission() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+        startActivityForResult(intent, SDCARD_ACCESS_PERMISSION_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (resultCode) {
+            Activity.RESULT_OK -> {
+                Toast.makeText(this, "何かが選択されました。", Toast.LENGTH_SHORT).show()
+                // TODO SDカードのフォルダ以外を選択肢た場合でもRESULT_OKが選択されるため、別途チェックが必要
+            }
+            else -> {
+                Log.d(TAG, "resultCode: $resultCode")
+            }
+        }
+    }
+
+    companion object {
+        private const val SDCARD_ACCESS_PERMISSION_REQUEST_CODE = 1
+        private val TAG = MainActivity::class.java.simpleName
     }
 }
